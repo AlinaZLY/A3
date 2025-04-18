8c9f58351adf451486f6acd489823be363131dab
siyuanWang348 <s.wang.73@student.scu.edu.au>
2025-04-18 17:38:32 +0800

    bug fixes

--- Assets/siyuanWang/Scripts/sherryCoinCollector.cs
+++ Assets/siyuanWang/Scripts/sherryCoinCollector.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class sherryCoinCollector : MonoBehaviour
 {
+    public GameObject window;
     public int coinCount = 0;              // 当前吃了多少
     public int totalCoins = 5;             // 场景中的金币总数
     public int maxCoinsCanEat = 5;         // 最多允许吃掉几个金币
@@ -20,6 +22,11 @@ public class sherryCoinCollector : MonoBehaviour
             coinCount++;
             Destroy(other.gameObject);     // 吃掉金币
             UpdateCoinUI();
+            if (coinCount >= 4)
+            {
+                window.SetActive(true);
+
+            }
         }
     }
 
