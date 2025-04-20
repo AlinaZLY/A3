5cd1278d6ddd4153ab03185a99b0a7b0c8f0ec97
siyuanWang348 <s.wang.73@student.scu.edu.au>
2025-04-20 16:10:17 +0800

    Increased countdown2

--- Assets/siyuanWang/Scripts/sherryCoinCollector.cs
+++ Assets/siyuanWang/Scripts/sherryCoinCollector.cs
@@ -5,10 +5,10 @@ using UnityEngine.UI;
 public class sherryCoinCollector : MonoBehaviour
 {
     public GameObject window;                // 成功窗口
-    public GameObject failWindow;            // 失败窗口（你说的 window fail 对象）
+    public GameObject failWindow;            // 失败窗口
     public int coinCount = 0;
-    public int totalCoins = 5;
-    public int maxCoinsCanEat = 5;
+    public int totalCoins = 4;               // 总金币改为4个
+    public int maxCoinsCanEat = 4;           // 最大可吃金币改为4个
     public TextMeshProUGUI coinText;
 
     public float timer = 60f;                // 倒计时
@@ -44,7 +44,8 @@ public class sherryCoinCollector : MonoBehaviour
 
             UpdateCoinUI();
 
-            if (coinCount >= 4 && !gameEnded)
+            // 修改成功条件为等于总金币数
+            if (coinCount >= totalCoins && !gameEnded)
             {
                 gameEnded = true;
                 window.SetActive(true);      // 成功窗口显示
@@ -63,7 +64,8 @@ public class sherryCoinCollector : MonoBehaviour
 
     void CheckFailCondition()
     {
-        if (coinCount < 4)
+        // 失败条件改为小于总金币数
+        if (coinCount < totalCoins)
         {
             gameEnded = true;
             failWindow.SetActive(true);      // 显示失败窗口
--- Assets/siyuanWang/Scripts/sherrytime.cs
+++ Assets/siyuanWang/Scripts/sherrytime.cs
@@ -4,50 +4,18 @@ using TMPro;
 public class sherrytime : MonoBehaviour
 {
     public float timeLeft = 60f; // 倒计时时间
-    public int coinsCollected = 0;
-    public int targetCoins = 4;
-
     public TextMeshProUGUI countdownText;
-    public TextMeshProUGUI resultText;
-
-    private bool gameEnded = false;
 
     void Update()
     {
-        if (gameEnded) return;
-
-        // 更新倒计时
-        timeLeft -= Time.deltaTime;
-        countdownText.text =  Mathf.Ceil(timeLeft).ToString();
-
-        if (timeLeft <= 0)
+        if (timeLeft > 0)
         {
-            EndGame(false);
-        }
-    }
-
-    public void CollectCoin()
-    {
-        if (gameEnded) return;
-
-        coinsCollected++;
-        if (coinsCollected >= targetCoins)
-        {
-            EndGame(true);
-        }
-    }
-
-    void EndGame(bool won)
-    {
-        gameEnded = true;
-        if (won)
-        {
-            resultText.text = "胜利！你吃到了4个金币！";
+            timeLeft -= Time.deltaTime;
+            countdownText.text = Mathf.Ceil(timeLeft).ToString();
         }
         else
         {
-            resultText.text = "失败！时间到了。";
+            countdownText.text = "时间到！";
         }
-        resultText.gameObject.SetActive(true);
     }
 }
